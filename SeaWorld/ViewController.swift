2db416d6a06ba5a682f25f6032a357c267a5cb8c
Rodionov Andrey <rodionov@sibext.com>
2019-09-11 22:22:03 +0700

    Initial Commit

---
+++ SeaWorld/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SeaWorld
+//
+//  Created by Andrey Rodionov on 11/09/2019.
+//  Copyright © 2019 Andrey. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
