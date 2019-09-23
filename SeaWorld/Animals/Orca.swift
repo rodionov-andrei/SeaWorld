c707c028a1cb13c70582f51dd5488523a1259169
Rodionov Andrey <rodionov@sibext.com>
2019-09-23 21:52:57 +0700

    Changed: orca can die before breed.

--- SeaWorld/Animals/Orca.swift
+++ SeaWorld/Animals/Orca.swift
@@ -19,24 +19,27 @@ final class Orca: Animal {
 
         turnsWithoutFeed += 1
         var modifiedCells = available
-
+        // Eat
         if let penguin = available.first(where: { $0.value is Penguin }) {
             modifiedCells.updateValue(nil, forKey: curr)
             modifiedCells.updateValue(currEntity, forKey: penguin.key)
             turnsWithoutFeed = 0
-        } else if turnsWithoutBreed >= breedPeriod, let spare = available.first(where: { $0.value == nil }) {
+            return modifiedCells
+        }
+        // Die?
+        if turnsWithoutFeed == maxTurnsWithoutFood {
+            modifiedCells.updateValue(nil, forKey: curr)
+            return modifiedCells
+        }
+        // Breed or move
+        if turnsWithoutBreed >= breedPeriod, let spare = available.first(where: { $0.value == nil }) {
             modifiedCells.updateValue(Orca(), forKey: spare.key)
             turnsWithoutBreed = 0
         } else if let spare = available.first(where: { $0.value == nil }) {
             modifiedCells.updateValue(currEntity, forKey: spare.key)
             modifiedCells.updateValue(nil, forKey: curr)
-        } else {
-            // surrounded by orcas
         }
 
-        if turnsWithoutFeed == 3 {
-            modifiedCells.updateValue(nil, forKey: curr)
-        }
         return modifiedCells
     }
 }
